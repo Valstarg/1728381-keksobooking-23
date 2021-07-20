861620bde9a5f4cb4063532021d646e175b98c5a
Valstarg <greshnicks@mail.ru>
2021-07-20 23:31:13 +0300

    Правда или действие (часть 1)

--- js/form.js
+++ js/form.js
@@ -4,6 +4,15 @@ const adForm = document.querySelector('.ad-form');
 const adFormList = adForm.querySelectorAll('fieldset');
 const mapFilters = document.querySelector('.map__filters');
 const selectForm = mapFilters.querySelectorAll('select');
+const titleInput = adForm.querySelector('#title');
+const priceInput = adForm.querySelector('#price');
+const roomNumber = document.querySelector('#room_number');
+const numberOfGuest = document.querySelector('#capacity');
+
+const TITLE_MIN = 30;
+const TITLE_MAX = 100;
+const PRICE_MAX = 1000000;
+const MAX_ROOMS = 100;
 
 // ФУНКЦИИ.
 
@@ -32,3 +41,37 @@ function makeActiveForm() {
   addDisabled(selectForm, false);
 }
 makeActiveForm();
+
+// Обработчик событий и валидация форм. Заголовок объявления.
+
+titleInput.addEventListener('input', () => {
+  const titleLength = titleInput.value.length;
+  if (titleLength < TITLE_MIN) {
+    titleInput.setCustomValidity(`Минимальный размер текста(с пробелами): ${TITLE_MIN - titleLength}`);
+  }
+  else if (titleLength > TITLE_MAX) {
+    titleInput.setCustomValidity(`Текст привышает допустимый размер. Удалите лишние символы: ${titleLength - TITLE_MAX}`);
+  }
+  else {
+    titleInput.setCustomValidity('');
+  }
+  titleInput.reportValidity();
+});
+
+// Цена за ночь.
+
+priceInput.setAttribute('max', PRICE_MAX);
+
+// Колличество комнат и мест.
+
+const defaultOptions = [...numberOfGuest.options];
+roomNumber.addEventListener('change', function () {
+  const selectedOption = this.options[this.selectedIndex];
+  numberOfGuest.innerHTML = '';
+  if (+selectedOption.value === MAX_ROOMS) {
+    numberOfGuest.append(defaultOptions[3]);
+  }
+  else {
+    numberOfGuest.append(...defaultOptions.filter((option) => option.value <= selectedOption.value & option.value > 0));
+  }
+});
